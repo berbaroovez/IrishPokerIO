c37c8de4d4d1e9e56769e664ef878ad5377ca057
berbaroovez <carterolson3@gmail.com>
2022-04-07 15:59:53 -0500

    updated url for sockets

--- server/src/index.ts
+++ server/src/index.ts
@@ -54,7 +54,7 @@ const activeRooms = [] as string[];
 
 const io = new Server(parseInt(process.env.PORT) || 3000, {
   cors: {
-    origin: "https://irish-io-next-test.vercel.app/",
+    origin: "https://irish-poker-io.vercel.app/",
     // origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
